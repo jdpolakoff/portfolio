f5a4e1987a9893608a88d9c488f63bb39acdccae
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-29 22:13:19 -0400

    added styles

--- js/jquery.js
+++ js/jquery.js
@@ -1,14 +1,22 @@
 // credit: http://burnmind.com/tutorials/how-to-create-a-typing-effect-an-eraser-effect-and-a-blinking-cursor-using-jquery
 
 
-function cursorAnimation() {
-    $('#cursor').animate({
-        opacity: 0
-    }, 'fast', 'swing').animate({
-        opacity: 1
-    }, 'fast', 'swing');
-}
+// function cursorAnimation() {
+//     $('#cursor').animate({
+//         opacity: 0
+//     }, 'fast', 'swing').animate({
+//         opacity: 1
+//     }, 'fast', 'swing')
+// }
 
 $(document).ready(function() {
-    setInterval ('cursorAnimation()', 1000);
-});
+    $('.stuff h1').typeIt({
+     strings: ["Jonathan Polakoff", "Web developer, journalist and"],
+     speed: 150,
+     html: true,
+     autoStart: true,
+     breaklines: true,
+     cursor: true,
+   })
+  //  setInterval ('cursorAnimation()', 1000)
+})
