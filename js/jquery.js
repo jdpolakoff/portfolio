d6938f452a9598a4222a7fdfb248900cb99443d0
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-09-08 14:04:09 -0400

    padding experiment

--- js/jquery.js
+++ js/jquery.js
@@ -1,13 +1,4 @@
-// credit: http://burnmind.com/tutorials/how-to-create-a-typing-effect-an-eraser-effect-and-a-blinking-cursor-using-jquery
 
-
-// function cursorAnimation() {
-//     $('#cursor').animate({
-//         opacity: 0
-//     }, 'fast', 'swing').animate({
-//         opacity: 1
-//     }, 'fast', 'swing')
-// }
 
 $(document).ready(function() {
     $('.stuff h1').typeIt({
